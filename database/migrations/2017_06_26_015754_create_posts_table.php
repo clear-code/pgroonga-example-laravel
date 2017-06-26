a0daf6ddb11719ff0f1662832a99799fe68e6452
YUKI Hiroshi <yuki@clear-code.com>
2017-06-26 01:59:11 +0000

    Add codes to create the posts table

--- database/migrations/2017_06_26_015754_create_posts_table.php
+++ database/migrations/2017_06_26_015754_create_posts_table.php
@@ -13,7 +13,13 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        //
+      Schema::create('posts', function(Blueprint $table) {
+        $table->increments('id');
+        $table->string('title');
+        $table->string('category_id');
+        $table->text('body');
+        $table->timestamps();
+      });
     }
 
     /**
@@ -23,6 +29,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        //
+      Schema::dropIfExists('posts');
     }
 }
