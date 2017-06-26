c327e0fe6cfc47cad9e4bd36e6e8fdf583e52ce7
YUKI Hiroshi <yuki@clear-code.com>
2017-06-26 02:19:50 +0000

    Implement "index" action for posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+  public function index(Request $request)
+  {
+    $posts = \App\Post::orderBy('id', 'desc')->get();
+    return \View::make('posts.index')
+             ->with('posts', $posts);
+  }
 }
